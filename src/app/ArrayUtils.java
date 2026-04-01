21b4a6cfe6a98dfd97ad48ac2dc76082030b0486
alex <aleksandrdenysov38@gmail.com>
2026-04-01 21:28:13 +0300

    Ready project

---
+++ src/app/ArrayUtils.java
@@ -0,0 +1,66 @@
+package app;
+
+public class ArrayUtils {
+
+
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) {
+            return;
+        }
+
+        int mid = array.length / 2;
+
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(array, left, right);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            }
+
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,35 @@
 package app;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my app");
+
+        int size = 10;
+        int[] array = new int[size];
+        Random random = new Random();
+
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(50);
+        }
+
+        System.out.println("Исходный массив: " + Arrays.toString(array));
+
+        ArrayUtils.mergeSort(array);
+
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
+
+
+
+        int target = array[random.nextInt(size)];
+        int index = ArrayUtils.binarySearch(array, target);
+
+        if (index != -1) {
+            System.out.println("Число " + target + " найдено по индексу: " + index);
+        } else {
+            System.out.println("Число не найдено");
+        }
     }
 }
